7b36bf840e3bda9b2aadfe21852529764ee77902
CreativeBinBag <nafisashamim1999@gmail.com>
2025-12-29 06:29:33 +0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,6 +15,6 @@ export default defineConfig({
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
   },
-  site: 'https://CreativeBinBag.github.io',
-  base: '/HaveYouSeenMyBlog', 
+  site: 'https://creativebinbag.github.io',
+  base: '/HaveYouSeenMyBlog/', 
 });
